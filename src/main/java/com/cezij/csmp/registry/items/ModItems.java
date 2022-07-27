438d5979cda3b06b6739941f466d9a1036f11260
Cezij <cscezij@gmail.com>
2022-07-27 14:59:13 +0200

    0.9.1-snapshot

--- src/main/java/com/cezij/csmp/registry/items/ModItems.java
+++ src/main/java/com/cezij/csmp/registry/items/ModItems.java
@@ -17,9 +17,10 @@ public class ModItems implements PolymerItem {
     public static final SimplePolymerItem GOLD_KYBER_CRYSTAL = new SimplePolymerItem(new Item.Settings().group(ItemGroup.TRANSPORTATION).fireproof(), Items.DIAMOND);
     public static final SimplePolymerItem GREEN_KYBER_CRYSTAL = new SimplePolymerItem(new Item.Settings().group(ItemGroup.TRANSPORTATION).fireproof(), Items.DIAMOND);
     public static final SimplePolymerItem ORANGE_KYBER_CRYSTAL = new SimplePolymerItem(new Item.Settings().group(ItemGroup.TRANSPORTATION).fireproof(), Items.DIAMOND);
-    public static final SimplePolymerItem PURPULE_KYBER_CRYSTAL = new SimplePolymerItem(new Item.Settings().group(ItemGroup.TRANSPORTATION).fireproof(), Items.DIAMOND);
+    public static final SimplePolymerItem PURPLE_KYBER_CRYSTAL = new SimplePolymerItem(new Item.Settings().group(ItemGroup.TRANSPORTATION).fireproof(), Items.DIAMOND);
     public static final SimplePolymerItem RED_KYBER_CRYSTAL = new SimplePolymerItem(new Item.Settings().group(ItemGroup.TRANSPORTATION).fireproof(), Items.DIAMOND);
     public static final SimplePolymerItem URANIUM_FRAGMENT = new SimplePolymerItem(new Item.Settings().group(ItemGroup.REDSTONE).food(ModFoodComponents.POISON),Items.NETHERITE_SCRAP);
+    public static final SimplePolymerItem KYBER_CRYSTAL = new SimplePolymerItem(new Item.Settings().group(ItemGroup.TRANSPORTATION).fireproof(), Items.DIAMOND);
 
 
 
@@ -34,7 +35,13 @@ public class ModItems implements PolymerItem {
         return null;
     }
     public static void registerItems(){
+        Registry.register(Registry.ITEM, new Identifier(SmpMod.MOD_ID,"kyber_crystal"),KYBER_CRYSTAL);
         Registry.register(Registry.ITEM, new Identifier(SmpMod.MOD_ID,"blue_kyber_crystal"),BLUE_KYBER_CRYSTAL);
+        Registry.register(Registry.ITEM, new Identifier(SmpMod.MOD_ID,"gold_kyber_crystal"),GOLD_KYBER_CRYSTAL);
+        Registry.register(Registry.ITEM, new Identifier(SmpMod.MOD_ID,"green_kyber_crystal"),GREEN_KYBER_CRYSTAL);
+        Registry.register(Registry.ITEM, new Identifier(SmpMod.MOD_ID,"orange_kyber_crystal"),ORANGE_KYBER_CRYSTAL);
+        Registry.register(Registry.ITEM, new Identifier(SmpMod.MOD_ID,"purple_kyber_crystal"),PURPLE_KYBER_CRYSTAL);
+        Registry.register(Registry.ITEM, new Identifier(SmpMod.MOD_ID,"red_kyber_crystal"),RED_KYBER_CRYSTAL);
         Registry.register(Registry.ITEM, new Identifier(SmpMod.MOD_ID,"uranium_ore"),URANIUM_ORE);
         Registry.register(Registry.ITEM, new Identifier(SmpMod.MOD_ID,"uranim_fragment"),URANIUM_FRAGMENT);
 
